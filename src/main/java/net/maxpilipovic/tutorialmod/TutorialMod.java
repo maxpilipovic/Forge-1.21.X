d04ddc30cebf72cb7765f7cbd30bbecc4a55df08
MaxPilipovic <maxpkusa@gmail.com>
2025-02-09 11:46:05 -0800

    EP 2 - ITEMS

--- src/main/java/net/maxpilipovic/tutorialmod/TutorialMod.java
+++ src/main/java/net/maxpilipovic/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.maxpilipovic.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.maxpilipovic.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -30,7 +32,9 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        //TEST
+        //Add Event Bus
+        ModItems.register(modEventBus);
+
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,6 +47,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
 
     }
 
